b7dd99ac41fd7854f347ecd1977e1af5c5f38624
MattDaruma <jerrymatthewmorris@icloud.com>
2023-02-18 22:14:53 -0700

    basic auth flow established

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket'
+import { CLIMessage, CommandService } from './command.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fiverealms-angular';
+  title = 'fiverealms-angular'
+  messages: CLIMessage[] = []
+  commandInput = new FormControl('')
+  constructor(public cms: CommandService) {
+    window.scrollTo(0, 0)
+    this.cms.message.subscribe(msg => {
+      this.messages.unshift(msg)
+      if(this.messages.length > 1000) this.messages.pop()
+    })
+  }
+  sendCommand(){
+    if(this.commandInput?.value) this.cms.send(this.commandInput?.value)
+    this.commandInput.reset()
+  }
 }
